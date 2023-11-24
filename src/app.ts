7af55b16f2e513889241c56ead8bcd573be97d9e
Abul Ala Jobayar <jobayar59@gmail.com>
2023-11-24 13:01:29 +0600

    setup-moongoes&envfile

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import express from 'express'
-const app = express()
-// const port = 5000
+import express from 'express';
+
+import cors from 'cors';
+const app = express();
+
+// middleware
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({
-    "success": true,
-    "message": "the server run successfully!",
-  })
-})
+    success: true,
+    message: 'the server run successfully!',
+  });
+});
 
-export default app
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
-import app from "./app"
-const port = 5000
+import mongoose from 'mongoose';
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+import app from './app';
+const port = 5000;
+
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+main();
